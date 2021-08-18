ec01b40a55fde3d1727745f7621b4d4b00e2b528
alan200276 <alan200276@gmail.com>
2021-08-18 21:25:28 +0800

    add pt slice

--- Cards/auto_run.sh
+++ Cards/auto_run.sh
@@ -18,14 +18,41 @@ do
    date +"%Y %b %m"
    date +"%r"
    
-# PT(H): 250 GeV ~ 500 GeV 
+# # PT(H): 250 GeV ~ 500 GeV 
+   
+#     echo "PP H hh"
+#     python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ggHj_250_500.txt > $outpath/proc_ggHj_250_500_"$i".log 
+
+#     echo "PP jj"
+#     python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ppjj_250_500.txt > $outpath/proc_ppjj_250_500_"$i".log
+    
+    
+# PT(H): 450 GeV ~ 700 GeV 
    
     echo "PP H hh"
-    python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ggHj.txt > $outpath/proc_ggHj_"$i".log 
+    python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ggHj_450_700.txt > $outpath/proc_ggHj_450_700_"$i".log 
 
-    echo "PP jjjj"
-    python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ppjj.txt > $outpath/proc_ppjj_"$i".log
+    echo "PP jj"
+    python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ppjj_450_700.txt > $outpath/proc_ppjj_450_700_"$i".log
 
+
+# PT(H): 650 GeV ~ 900 GeV 
+   
+    echo "PP H hh"
+    python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ggHj_650_900.txt > $outpath/proc_ggHj_650_900_"$i".log 
+
+    echo "PP jj"
+    python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ppjj_650_900.txt > $outpath/proc_ppjj_650_900_"$i".log
+    
+    
+    
+# PT(H): 850 GeV ~ 1100 GeV 
+   
+    echo "PP H hh"
+    python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ggHj_850_1100.txt > $outpath/proc_ggHj_850_1100_"$i".log 
+
+    echo "PP jj"
+    python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ppjj_850_1100.txt > $outpath/proc_ppjj_850_1100_"$i".log
 
    
    date +"%Y %b %m"
@@ -36,6 +63,7 @@ done
 
 
 gzip -d $mcdatapath/proc_*/Events/run_*/unweighted_events.lhe.gz
+gzip -d $mcdatapath/proc_*/Events/run_*/events.lhe.gz
 
 
 echo "Finish"
