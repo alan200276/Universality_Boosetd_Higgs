37b4b8fffc841fa028fd8793269178e35db509e8
alan200276 <alan200276@gmail.com>
2021-08-19 14:12:06 +0800

    add ehco pt slices

--- Cards/auto_run.sh
+++ Cards/auto_run.sh
@@ -20,39 +20,39 @@ do
    
 # # PT(H): 250 GeV ~ 500 GeV 
    
-#     echo "PP H hh"
-#     python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ggHj_250_500.txt > $outpath/proc_ggHj_250_500_"$i".log 
+    echo "ggH pt 250 GeV ~ 500 GeV"
+    python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ggHj_250_500.txt > $outpath/proc_ggHj_250_500_"$i".log 
 
-#     echo "PP jj"
+#     echo "PP jj pt 250 GeV ~ 500 GeV"
 #     python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ppjj_250_500.txt > $outpath/proc_ppjj_250_500_"$i".log
     
     
 # PT(H): 450 GeV ~ 700 GeV 
-   
-    echo "PP H hh"
+
+    echo "ggH pt 650 GeV ~ 900 GeV"
     python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ggHj_450_700.txt > $outpath/proc_ggHj_450_700_"$i".log 
 
-    echo "PP jj"
-    python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ppjj_450_700.txt > $outpath/proc_ppjj_450_700_"$i".log
+#     echo "PP jj pt 450 GeV ~ 700 GeV"
+#     python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ppjj_450_700.txt > $outpath/proc_ppjj_450_700_"$i".log
 
 
 # PT(H): 650 GeV ~ 900 GeV 
-   
-    echo "PP H hh"
+
+    echo "ggH pt 50 GeV ~ 900 GeV"
     python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ggHj_650_900.txt > $outpath/proc_ggHj_650_900_"$i".log 
 
-    echo "PP jj"
-    python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ppjj_650_900.txt > $outpath/proc_ppjj_650_900_"$i".log
+#     echo "PP jj pt 650 GeV ~ 900 GeV"
+#     python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ppjj_650_900.txt > $outpath/proc_ppjj_650_900_"$i".log
     
     
     
 # PT(H): 850 GeV ~ 1100 GeV 
    
-    echo "PP H hh"
+    echo "ggH pt 850 GeV ~ 1100 GeV"
     python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ggHj_850_1100.txt > $outpath/proc_ggHj_850_1100_"$i".log 
 
-    echo "PP jj"
-    python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ppjj_850_1100.txt > $outpath/proc_ppjj_850_1100_"$i".log
+#     echo "PP jj"
+#     python /root/MG5_aMC_v2_7_3/bin/mg5_aMC $cardpath/proc_ppjj_850_1100.txt > $outpath/proc_ppjj_850_1100_"$i".log
 
    
    date +"%Y %b %m"
@@ -62,8 +62,19 @@ do
 done
 
 
-gzip -d $mcdatapath/proc_*/Events/run_*/unweighted_events.lhe.gz
-gzip -d $mcdatapath/proc_*/Events/run_*/events.lhe.gz
+{ # try
+    
+    gzip -d $mcdatapath/proc_*/Events/run_*/unweighted_events.lhe.gz
+
+
+} || { # catch
+
+
+    gzip -d $mcdatapath/proc_*/Events/run_*/events.lhe.gz
+}
+
+
+
 
 
 echo "Finish"
